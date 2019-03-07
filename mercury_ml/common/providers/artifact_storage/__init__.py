105778d23dc618f8fa135ead5f0f94b11e089b24
Karl Schriek <Karl.Schriek@alexanderthamm.com>
2019-03-07 14:33:54 +0100

    MongoDBSingleton now only imports pymongo from within the __init__ scope

--- mercury_ml/common/providers/artifact_storage/__init__.py
+++ mercury_ml/common/providers/artifact_storage/__init__.py
@@ -1,8 +1,9 @@
 """Classes that facilitate moving data from one store into another"""
 from mercury_ml.common.utils import singleton
-from pymongo import MongoClient
+
 
 @singleton
 class MongoClientSingleton:
     def __init__(self, **kwargs):
+        from pymongo import MongoClient
         self.client = MongoClient(**kwargs)
